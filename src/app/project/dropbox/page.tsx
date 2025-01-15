e5aa71ad670e087d439445005d2c92f6c1a5e008
Tae young Kim <tyk0707@gmail.com>
2025-01-15 12:10:43 +0900

    1200

--- src/app/project/dropbox/page.tsx
+++ src/app/project/dropbox/page.tsx
@@ -1,5 +1,5 @@
 
-'use client';
+'use client'; 
 
 import DropboxImageList from "@/components/dropbox/dropbox-image-list";
 import FileDragDropZone from "@/components/dropbox/file-dragdropzone";
@@ -11,16 +11,16 @@ export default function Page() {
 
     const [ searchInput, setSearchInput ] = useState('');
     
-    return (
+    return ( 
         <div className="container mx-auto flex px-5 py-20 pb-5 md:flex-row flex-col items-center">
-            <div className="w-full flex-grow flex flex-col">
-                <div className="flex justify-between mr-40">
-                    <Logo />
+            <div className="w-full flex-grow flex flex-col"> 
+                <div className="flex justify-between mr-40"> 
+                    <Logo /> 
                     <SearchComponent 
                         searchInput={searchInput}
                         setSearchInput={setSearchInput} />
-                </div>
-                <FileDragDropZone />
+                </div> 
+                <FileDragDropZone /> 
                 <DropboxImageList searchInput={searchInput} />
             </div>
         </div>
