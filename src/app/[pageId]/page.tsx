c46a03a0dbf1b36e264d86afbd3b7518641b1a2d
Tae young Kim <tyk0707@gmail.com>
2025-01-16 10:53:27 +0900

    1100

--- src/app/[pageId]/page.tsx
+++ src/app/[pageId]/page.tsx
@@ -8,21 +8,19 @@ interface PageParams {
     };
 };
 
-export default async function Page({ params }: Promise<PageParams>) {
-    const notion = new NotionAPI();
+export default async function Page({ params }: PageParams) {
  
-    // const { pageId } = params;
+    const { pageId } = params;
 
-    // if (!pageId)
-    //     return <div>잘못된 페이지 ID입니다.</div>;
+    if (!pageId)
+        return <div>잘못된 페이지 ID입니다.</div>;
 
     try {
-            const { pageId } = await params;
-            const recordMap = await notion.getPage(pageId); // 세부 페이지 데이터 가져오기
-            
-            return <Render recordMap={recordMap} />;  //데이터를 Render 컴포넌트에 전달
-        } catch (error) {
-            console.error(error);
-            return <div>페이지를 불러오는 데 오류가 발생했습니다.</div>;
-        }
+        const notion = new NotionAPI();
+        const recordMap = await notion.getPage(pageId); // 세부 페이지 데이터 가져오기
+
+        return <Render recordMap={recordMap} />;  //데이터를 Render 컴포넌트에 전달
+    } catch (error) {
+        return <div>페이지를 불러오는 데 오류가 발생했습니다.</div>;
+    }
 };
