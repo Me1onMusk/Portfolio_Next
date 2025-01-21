4dcd97364b1c3f82e9fa90cff5443d1d943d835c
Tae young Kim <tyk0707@gmail.com>
2025-01-21 10:50:40 +0900

    1100

--- src/app/project/diary/page.tsx
+++ src/app/project/diary/page.tsx
@@ -6,18 +6,13 @@ import DiaryList from "@/components/diary/diary-list";
 import Header from "@/components/diary/header";
 
 export default function Page() {
-
-    const onClick = () => {
-        
-    };
-
     return (
         <div className="container w-full mx-auto p-20 flex flex-col items-center justify-center">
             <div>
                 <Header 
-                    leftChild={ <Button text={'<'} type={''} onClick={ onClick } /> } 
+                    leftChild={ <Button text={'<'} type={''} onClick={()=>{}} /> } 
                     title={ new Date().toLocaleDateString() } 
-                    rightChild={ <Button text={'>'} type={''} onClick={ onClick } /> } > 
+                    rightChild={ <Button text={'>'} type={''} onClick={()=>{}} /> } > 
                 </Header>
                 <DiaryList />
             </div>
