a452241d3ae05ddd98302219994250284c17c3a0
Tae young Kim <tyk0707@gmail.com>
2025-02-03 11:24:47 +0900

    1130

--- src/app/project/emotion-diary/layout.tsx
+++ src/app/project/emotion-diary/layout.tsx
@@ -1,10 +1,6 @@
 
 import DiaryProvider from "./DiaryProvider"; // 위에서 만든 파일
 
-export const metadata = {
-    title: "Emotion Diary",
-};
-
 export default function Page({children}: Readonly<{children: React.ReactNode;}>) {
     return (
         <>
