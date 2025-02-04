35f1b857211f8afd624b5196da7017827268e6e1
Tae young Kim <tyk0707@gmail.com>
2025-02-04 14:43:23 +0900

    1430

--- src/app/project/bookstore/(with-searchbar)/search/page.tsx
+++ src/app/project/bookstore/(with-searchbar)/search/page.tsx
@@ -2,14 +2,6 @@
 import BookItem from '@/components/bookstore/book-item'; 
 import { BookData } from '../../../../../../types'; 
 
-interface PageProps {
-    // Next.js에서는 searchParams가 기본적으로 string 또는 string[]일 수 있기 때문에
-    // 필요하다면 아래처럼 좀 더 유연하게 작성할 수 있습니다.
-    searchParams?: {
-      q?: string; // 실제로는 q가 없을 수도 있고, 문자열 배열일 수도 있음
-    };
-  }
-
 async function SearchResult ({q}: {q :string}) {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`); 
     if(!response.ok) 
@@ -26,7 +18,7 @@ async function SearchResult ({q}: {q :string}) {
 };
 
 // ~/search?q=단어 //
-export default function Page({searchParams} : PageProps) {
+export default function Page({searchParams} : any) {
     return (    
         <>  
             <SearchResult q = {searchParams.q || ""} /> 
